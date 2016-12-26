aaf4d97c55eaf6dd65503cce7117fb2b6f4c15ce
Carl Halstead <taleof4gamers2012@gmail.com>
2016-12-26 20:39:25 +0000

    Added support for simple faction stats
    These include name, ID, best chain, leader ID etc.

---
+++ FactionStatistics.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TornCityAPISharp
+{
+	public class FactionStatistics
+	{
+		public enum FactionData
+		{
+			ID,
+			name,
+			leader,
+			best_chain,
+			respect,
+			rank
+		}
+
+		private static string generalStatsUrl = "https://api.torn.com/faction/?selections=&key=";
+
+		/// <summary>
+		/// The URL for the general stats
+		/// </summary>
+		public static string URL
+		{
+			get { return (generalStatsUrl + API.apiKey); }
+		}
+
+		/// <summary>
+		/// This retrieves inforamtion about ALL available statistics for the WHOLE of Torn, asynchronously
+		/// </summary>
+		/// <returns></returns>
+		public static async Task<string> GetSimpleFactionStatisticsAsync(FactionData fd)
+		{
+			var t = await Task<string>.Factory.StartNew(() => GetSimpleFactionStatistics(fd));
+			return t;
+		}
+
+		/// <summary>
+		/// This retrieves inforamtion about ALL available statistics for the WHOLE of Torn
+		/// </summary>
+		/// <returns></returns>
+		[Obsolete("Consider using the Async version within an Async method using await when calling the function")]
+		public static string GetSimpleFactionStatistics(FactionData fd)
+		{
+			if (API.DoesAPIKeyExist() == false) {
+				return null;
+			}
+
+			using (WebClient wc = new WebClient())
+			{
+				var json = wc.DownloadString(URL);
+				var data = JsonConvert.DeserializeObject<Dictionary<object, object>>(json);
+				return data[fd.ToString()].ToString();
+			}
+		}
+	}
+}
