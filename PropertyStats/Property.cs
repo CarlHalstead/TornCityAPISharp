3cfbdf1aba5691f5a9fd3144e87f36bad8c8928f
lvl <lode_vl@hotmail.com>
2016-12-29 11:43:26 +0100

    Full Property Support Added

---
+++ PropertyStats/Property.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TornCityAPISharp.PropertyStats
+{
+    public class Property
+    {
+        [JsonProperty("owner_id")]
+        public int OwnerId { get; set; }
+
+        [JsonProperty("property_type")]
+        public int TropertyType { get; set; }
+
+        [JsonProperty("happy")]
+        public int Happy { get; set; }
+
+        [JsonProperty("upkeep")]
+        public int Upkeep { get; set; }
+
+        [JsonProperty("upgrades")]
+        public List<string> Upgrades { get; set; }
+
+        [JsonProperty("staff")]
+        public List<string> Staff { get; set; }
+
+        [JsonProperty("rented")]
+        public Rented Rented { get; set; }
+
+        [JsonProperty("users_living")]
+        public string UsersLiving { get; set; }
+    }
+}
---
+++ PropertyStats/PropertyProperty.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TornCityAPISharp.PropertyStats
+{
+    public class PropertyProperty : IPropertyStatistics
+    {
+        [JsonProperty("property")]
+        public Property Property { get; set; }
+
+        public string GetMethodName()
+        {
+            return PropertyMethods.property.ToString();
+        }
+    }
+}
