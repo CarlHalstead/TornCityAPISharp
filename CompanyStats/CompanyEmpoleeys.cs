777a426806d85d6fb02c9a5a9fd871cf6b887193
lvl <lode_vl@hotmail.com>
2017-02-23 15:01:07 +0100

    Fixing typo and making property public

--- CompanyStats/CompanyEmpoleeys.cs
+++ CompanyStats/CompanyEmpoleeys.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 
 namespace TornCityAPISharp.CompanyStats
 {
-    public class CompanyEmpoleeys: ICompanyStatistics
+    public class CompanyEmpolyees: ICompanyStatistics
     {
         /// <summary>
         /// Value of the Company employees node of the json response (renamed because of it would have been abigous with the class name)
         /// </summary>
         [JsonProperty("company_employees")]
-        Dictionary<long, EmployeeDetails> CompanyEmployees { get; set; }
+        public Dictionary<long, EmployeeDetails> CompanyEmployees { get; set; }
 
         public string GetMethodName()
         {
